7585ed127a1b1534e1f51f2eedb0e6f71a7e369d
Yassine OUAISSA <ouaissa24@gmail.com>
2018-11-07 11:54:28 +0100

    Add Projet folder

--- Projet_ESP/FFT_ESP_Server.py
+++ Projet_ESP/FFT_ESP_Server.py
@@ -1,25 +1,46 @@
 import socket as sock
-import matplotlib.pyplot as p
+import matplotlib.pyplot as plt
+import numpy as np 
+import matplotlib.animation as ani
+
+
+Data = []
+
+def strToFloat(str):
+    for i in range(len(str)):
+        str[i] = float(str[i])
 
 def receve_DATA():
     
     socket = sock.socket(sock.AF_INET,sock.SOCK_STREAM)
     socket.bind(('192.168.4.2',1555))
-    
-    while True:
-        socket.listen(0)
-        client, adress = socket.accept()
-        print("{} Connected ".format(adress))
-        rec = client.recv(9999999)
-        print(rec)
-        if "END" in rec:
-            break
+    socket.listen(0)    
+    client, adress = socket.accept()
+    print("{} Connected ".format(adress))
+    rec = client.recv(99999999,sock.MSG_WAITALL)
+        #print(rec)
+        #if "END" in rec:
+         #   break
     data = rec.split(';')
     del data[-1]
-    
     print("[DEBUG]Close Connection")
     client.close()
     socket.close()
     return data
 
-print(receve_DATA())
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+
+
+def update_plot(yData):
+    try:
+        yData = receve_DATA()
+        strToFloat(yData)
+    except Exception as e :
+        print(e)
+    finally:
+        ax.clear()
+        ax.plot(yData,'r')
+        
+plot_ani = ani.FuncAnimation(fig,update_plot,fargs=(Data),interval = 1)
+plt.show()
